2460cd456521005f71ae4881a4211a6b534e54c9
wishtoday238 <wishtoday238@gmail.com>
2025-12-05 17:52:08 +0800

    修复客户端没有正确注册问题(我在干嘛,我怎么把客户端的代码删掉了)

--- src/main/java/com/wishtoday/packetregister/client/PacketregisterClient.java
+++ src/main/java/com/wishtoday/packetregister/client/PacketregisterClient.java
@@ -1,10 +1,16 @@
 package com.wishtoday.packetregister.client;
 
+import com.wishtoday.packetregister.Packetregister;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 
 public class PacketregisterClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
+        ClientLifecycleEvents.CLIENT_STARTED.register((client) -> {
+            Packetregister.startScan(EnvType.CLIENT);
+        });
     }
 }
